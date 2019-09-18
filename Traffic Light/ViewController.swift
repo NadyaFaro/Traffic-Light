b3c390826db921ae84578495987f1f5417760eee
Max <max@maxs-imac.local>
2019-09-18 22:17:36 +0300

    Added outlets and functions

--- Traffic Light/ViewController.swift
+++ Traffic Light/ViewController.swift
@@ -8,13 +8,50 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+enum LightColour {
+    case red, yellow, green
+}
 
+class ViewController: UIViewController {
+    
+    private var lightColour: LightColour = .red
+    
+    @IBOutlet var redLight: UIView!
+    @IBOutlet var yellowLight: UIView!
+    @IBOutlet var greenLight: UIView!
+    @IBOutlet var startButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        redLight.layer.cornerRadius = redLight.frame.width / 1.5
+        yellowLight.layer.cornerRadius = yellowLight.frame.width / 1.5
+        greenLight.layer.cornerRadius = greenLight.frame.width / 1.5
+        
+        redLight.alpha = 0.3
+        yellowLight.alpha = 0.3
+        greenLight.alpha = 0.3
+        
     }
 
-
+    @IBAction func pressStartButton() {
+        startButton.setTitle("NEXT", for: .normal)
+        
+        switch lightColour {
+        case .red:
+            redLight.alpha = 1
+            greenLight.alpha = 0.3
+            lightColour = .yellow
+        case .yellow:
+            redLight.alpha = 0.3
+            yellowLight.alpha = 1
+            lightColour = .green
+        case .green:
+            yellowLight.alpha = 0.3
+            greenLight.alpha = 1
+            lightColour = .red
+        }
+    }
+    
 }
 
